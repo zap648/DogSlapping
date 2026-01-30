160c4d6bf28ab9073a0afca1911733abbb76d892
potatce <147557507+potatce@users.noreply.github.com>
2026-01-30 23:38:22 +0100

    Main Menu

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject startCanvas;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TriggerButton();
+    }
+
+    void TriggerButton()
+    {
+        if (Input.anyKeyDown && startCanvas.activeSelf) StartGame();
+        if (Input.anyKeyDown && !startCanvas.activeSelf) LoseGame();
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Clicked button");
+        startCanvas.SetActive(false);
+    }
+
+    public void LoseGame()
+    {
+        Debug.Log("You are murderer");
+        startCanvas.SetActive(true);
+    }
+}
